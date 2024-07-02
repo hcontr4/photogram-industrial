2ec676c2cd42adbd515d7a45537b51b2d1546e89
Herman Contreras <contrerasherman2@gmail.com>
2024-07-02 10:33:21 -0500

    Resolved Thierry's comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ApplicationRecord
 
   has_many :likes, foreign_key: :fan_id
 
-  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :sent_follow_requests, dependent: :destroy, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
 
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
