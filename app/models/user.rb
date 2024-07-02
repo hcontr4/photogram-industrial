114f13da89a8c041ca8b5d2648390036a53ad9ce
Herman Contreras <contrerasherman2@gmail.com>
2024-07-02 10:11:01 -0500

    Created sample_data task

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,16 +28,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :own_photos, class_name: "Photo", foreign_key: "ownner_id"
+  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
   has_many :comments, foreign_key: "author_id"
 
   has_many :likes, foreign_key: :fan_id
 
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
-  has_many :accepted_sent_follow_requests, -> { where(status: "accepted") }, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
 
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
-  has_many :accepted_follow_requests, -> { where(status: "accepted") }, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :accepted_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
 
   has_many :liked_photos, through: :likes, source: :photo
 
