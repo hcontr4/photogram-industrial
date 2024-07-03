1c4854cd854b4dfc3f16491de7bfd829f6153b7f
Herman Contreras <contrerasherman2@gmail.com>
2024-07-03 16:51:15 -0500

    Add feed route and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :liked, :feed]
+
   def show
-    @user = User.find_by!(username: params.fetch(:username))
+   
   end
 
   def liked
-    @user = User.find_by!(username: params.fetch(:username))
+  
   end
+
+  def feed
+    
+  end
+
+  def followers
+
+  end
+end
+
+private
+
+def set_user
+  @user = User.find_by!(username: params.fetch(:username))
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "photos#index"
-  
+
   devise_for :users
 
   resources :likes
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   resources :photos
 
   get ":username/liked" => "users#liked", as: :liked
+  get ":username/feed" => "users#feed", as: :feed
+  get ":username/followers" => "users#followers", as: :followers
+  # get ":username/following"
 
   get ":username" => "users#show", as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
